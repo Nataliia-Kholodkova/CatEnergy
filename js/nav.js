69139af3bfb8fdd03814fa4d5ef16ab77930cec2
UA_Girl <n_kholodkova@ukr.net>
2021-02-25 21:32:53 +0200

    Added index, catalog, form, styles and JS

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,11 @@
 (function () {
     function setNavigation () {
+        document.querySelector('.header').classList.remove('header__noJS');
         const nav = document.querySelector('.nav');
         const navButton = document.querySelector('.btn-nav');
         const navList = document.querySelector('.nav-list');
         if (window.matchMedia("(max-width: 767px)").matches) {
-            nav.classList.remove('header-nav__noJS');
+
             navList.classList.add('nav-list__closed');
             navButton.addEventListener('click', function (event) {
                 event.preventDefault();
